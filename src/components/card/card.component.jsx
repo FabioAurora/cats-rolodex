366a0f8eeb1aed87e7adf443a7c0e4ec5bdbeb3e
Fabio Aurora <fabioauroradev@gmail.com>
2023-04-03 23:54:37 +0100

    feat: adding React app to gh pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ export default class Card extends Component {
   render() {
     const {name, id, email} = this.props.cat;
     return (
-      <div className="card-container">
+      <div className="card-container" key={id}>
         <img src={`https://robohash.org/${id}?set=set4&size=180x180`} alt={`cat ${name}`} />
         <h2>{name}</h2>
         <p>{email}</p>
